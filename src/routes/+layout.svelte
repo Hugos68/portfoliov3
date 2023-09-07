80ae20dd98339c9f89319cd70c9c875589f3047b
Hugo <hugokorteapple@gmail.com>
2023-09-07 23:46:03 +0200

    Implemented view transition for active page underline

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
+	import { onNavigate } from '$app/navigation';
 	import '../app.postcss';
 	import Header from './Header.svelte';
+
+	onNavigate(() => {
+		if (!document.startViewTransition) return;
+		return new Promise((fulfil) => document.startViewTransition(() => new Promise(fulfil)));
+	});
 </script>
 
 <Header />
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,10 +1,24 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
 <header
 	class="bg-white text-sunset-orange-500 font-semibold text-lg sticky top-0 p-6 rounded-tl-3xl rounded-bl-md rounded-br-3xl rounded-tr-md flex gap-8"
 >
 	<nav class="flex gap-[5vw] mx-auto">
-		<a href="/">HK</a>
-		<a href="/about">About</a>
-		<a href="/work">Work</a>
-		<a href="/contact">Contact</a>
+		<a class:active={$page.url.pathname === '/'} href="/">HK</a>
+		<a class:active={$page.url.pathname === '/about'} href="/about">About</a>
+		<a class:active={$page.url.pathname === '/work'} href="/work">Work</a>
+		<a class:active={$page.url.pathname === '/contact'} href="/contact">Contact</a>
 	</nav>
 </header>
+
+<style>
+	.active {
+		@apply relative;
+	}
+	.active::before {
+		@apply content-[''] bg-sunset-orange-500 h-1 w-full absolute bottom-0;
+		view-transition-name: indicator;
+	}
+</style>
