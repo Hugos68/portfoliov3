3ea7ff877c10209ba4b1fc3215d133f1b5dee3ae
Hugos68 <hugokorteapple@gmail.com>
2023-09-08 19:08:50 +0200

    added more hero page stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,22 @@
-<section class="mt-[10vh] flex justify-evenly items-center">
-	<div class="w-min text-end">
+<section class="mt-[20vh] flex flex-col md:flex-row justify-evenly items-center">
+	<div class="md:w-min text-end">
 		<p class="text-yellow-200 font-bold tracking-widest p-0">Fullstack Developer</p>
 		<h1 class="text-[clamp(2.75rem,10vw,8rem)] leading-tight font-extrabold">Hugo Korte</h1>
 	</div>
-	<div>
-		<i class="fa-brands fa-java" />
-		<i class="fa-brands fa-js" />
-		<i class="fa-brands fa-c#" />
-		<img
-			class="h-96 rounded-tl-md rounded-bl-[80px] rounded-br-md rounded-tr-[80px]"
-			src="/hugosketch.png"
-			alt="profile"
-		/>
-	</div>
+	<img
+		class=" hidden md:block h-48 lg:h-64 xl:h-96 rounded-tl-md rounded-bl-[80px] rounded-br-md rounded-tr-[80px]"
+		src="/hugo.jpg"
+		alt="profile"
+	/>
 </section>
+
+<style lang="postcss">
+	img {
+		box-shadow: 0 0 5px 5px theme('colors.sunset-orange.500'),
+			0 0 0 10px theme('colors.sunset-orange.400'), 0 0 0 15px theme('colors.sunset-orange.500'),
+			0 0 0 20px theme('colors.sunset-orange.400'), 0 0 0 25px theme('colors.sunset-orange.500'),
+			0 0 0 30px theme('colors.sunset-orange.400'), 0 0 0 35px theme('colors.sunset-orange.500'),
+			0 0 0 40px theme('colors.sunset-orange.400'), 0 0 0 45px theme('colors.sunset-orange.500'),
+			0 0 0 50px theme('colors.sunset-orange.400');
+	}
+</style>
